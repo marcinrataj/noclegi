e52a17eda8b7b6ea269757261e7c9aef0ac0e729
Marcin Rataj <marcinrataj0@gmail.com>
2024-02-20 10:27:44 +0100

    actual state before start create hook

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// useState();
+
+function TestHook() {
+  return(
+    <>
+    <h1>Test hook</h1>
+    <input type='text'/>
+    </>
+  )
+}
+
 const rootNode = document.getElementById('root');
+// ReactDOM.render(<TestHook />, rootNode);
 ReactDOM.render(<App />, rootNode);
 
 // If you want to start measuring performance in your app, pass a function
